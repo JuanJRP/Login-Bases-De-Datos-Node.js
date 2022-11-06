01160cba26ab14baed4514b0ca850f1756edbee6
Juan <juanjoserestrepopabon@gmail.com>
2022-11-06 17:02:02 -0500

    v1.2

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const engine = require('ejs-mate');
 const path = require('path');
 const morgan = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
 
 const app = express();
 require('./db');
+require('./passport/local-auth');
 
 app.set('views', path.join(__dirname, 'views'));
 app.engine('ejs', engine);
@@ -13,6 +16,8 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', require('./routes/index'));
 
