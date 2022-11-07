730c1f5824a92dfb186cf2edf3ac4bf39926b8bf
Juan <juanjoserestrepopabon@gmail.com>
2022-11-06 19:55:41 -0500

    v1.4

--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt-nodejs');
+
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
     email: String,
-    password: String,
+    password: String
 });
 
 userSchema.methods.encryptPassword = (password) => {
@@ -12,7 +13,7 @@ userSchema.methods.encryptPassword = (password) => {
 };
 
 userSchema.methods.comparePassword = function(password) {
-    return bcrypt.compareSync(password, this.password)
-}
+    return bcrypt.compareSync(password, this.password);
+};
 
-module.exports = mongoose.model('users', userSchema);
+module.exports = mongoose.model('user', userSchema);
